24ec8746353109bb4b45329b48fb96c44ee06e59
ibuildthehealth <ibuildthehealth@easylifewell.com>
2016-12-11 21:59:25 +0800

    详情页

--- client/src/app/components/Present.js
+++ client/src/app/components/Present.js
@@ -7,16 +7,17 @@
 import React from 'react';
 import {Component} from 'react';
 import {Link, browserHistory} from 'react-router';
-import { Icon, Card, Button, Checkbox } from 'antd';
+import { Icon, Card, Button, Carousel } from 'antd';
 import moment from 'moment';
 import _ from 'lodash';
-import {presentMaterial} from './config';
+import {presentMaterial, detailTab} from './config';
 
 export default class Present extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            currentType: 'old'
+            currentType: 'old',
+            currentTab: 'detail'
         }
     }
 
@@ -26,11 +27,18 @@ export default class Present extends Component {
         });
     };
 
+    goToDetail = (id) => {
+        browserHistory.push('/present?id=' + id);
+        this.setState({
+            currentTab: 'detail'
+        });
+    };
+
     renderPresentList = () => {
         let {currentType} = this.state;
         let currentTypeItem = _.find(presentMaterial, {type: currentType});
         return currentTypeItem.materialList.map((item, index) => {
-            return <Card key={index}>
+            return <Card key={index} onClick={this.goToDetail.bind(this, item.id)}>
                 <span className="img"
                     style={{backgroundImage: `url(${item.imgSrc})`}}>
                 </span>
@@ -57,15 +65,73 @@ export default class Present extends Component {
         });
     };
 
+    renderCarousel = (carouselMaterial) => {
+        return carouselMaterial.map((item, index) => {
+            return <div key={index}>
+                <Link to={item.href}>
+                    <img src={item.imgSrc} />
+                </Link>
+            </div>
+        });
+    };
+
+    changeDetailTab = (tab) => {
+        this.setState({
+            currentTab: tab
+        });
+    };
+
+    renderTabs = () => {
+        let {currentTab} = this.state;
+        return detailTab.map((item, index) => {
+            return <span
+                className={currentTab === item.key ? 'current' : ''}
+                key={index}
+                onClick={this.changeDetailTab.bind(this, item.key)}>
+                {item.title}
+            </span>;
+        });
+    };
+
+    renderPresentDetail = () => {
+        let {id} = this.props.location.query;
+        let {currentType, currentTab} = this.state;
+        let {materialList} = _.find(presentMaterial, {type: currentType});
+        let currentPresentItem = _.find(materialList, {id: Number(id)});
+        let {detail} = currentPresentItem;
+        let imageSrc = detail[`${currentTab}ImageSrc`];
+        return <div>
+            <div className="present-title">{currentPresentItem.name}：{detail.contain}</div>
+            <Carousel autoplay>
+                {this.renderCarousel(detail.carouselMaterial)}
+            </Carousel>
+            <div className="detail-tab">
+                {this.renderTabs()}
+            </div>
+            <div className="detail-tab-content">
+                <img src={`${imageSrc}`} width="100%" />
+            </div>
+        </div>;
+    };
+
     render() {
+        let {id} = this.props.location.query;
         return (
             <div className="present">
-                <div className="present-tab-nav">
-                    {this.renderPresentNav()}
-                </div>
-                <div className="present-list">
-                    {this.renderPresentList()}
-                </div>
+                {
+                    !id
+                    ? <div className="present-resume">
+                        <div className="present-tab-nav">
+                            {this.renderPresentNav()}
+                        </div>
+                        <div className="present-list">
+                            {this.renderPresentList()}
+                        </div>
+                    </div>
+                    : <div className="present-detail">
+                        {this.renderPresentDetail()}
+                    </div>
+                }
             </div>
         );
     }
--- client/src/app/components/config.js
+++ client/src/app/components/config.js
@@ -19,6 +19,20 @@ module.exports = {
             href: ''
         }
     ],
+    detailTab: [
+        {
+            title: '商品详情',
+            key: 'detail'
+        },
+        {
+            title: '商品认证',
+            key: 'identify'
+        },
+        {
+            title: '追根溯源',
+            key: 'resource'
+        }
+    ],
     presentMaterial: [
         {
             title: '敬老坊',
@@ -29,19 +43,32 @@ module.exports = {
                     name: '老金磨坊',
                     imgSrc: '/image/assets/present/old/1.png',
                     price: 998,
-                    origin: '澳洲'
-                },
-                {
-                    id: 2,
-                    name: '老金磨坊2',
-                    imgSrc: '/image/assets/present/old/2.png',
-                    price: 999,
-                    origin: '澳洲'
+                    origin: '澳洲',
+                    detail: {
+                        carouselMaterial: [
+                            {
+                                imgSrc: '/image/assets/present/old/1/carousel/1.png',
+                                href: ''
+                            },
+                            {
+                                imgSrc: '/image/assets/present/old/1/carousel/2.png',
+                                href: ''
+                            },
+                            {
+                                imgSrc: '/image/assets/present/old/1/carousel/3.png',
+                                href: ''
+                            }
+                        ],
+                        contain: '鱼油（1）＋葡萄籽（2）',
+                        detailImageSrc: '/image/assets/present/old/1/detail.png',
+                        identifyImageSrc: '/image/assets/present/old/1/identify.png',
+                        resourceImageSrc: '/image/assets/present/old/1/resource.png'
+                    }
                 }
             ]
         },
         {
-            title: '爱幼坊',
+            title: '育婴坊',
             type: 'child',
             materialList: [
                 {
@@ -49,14 +76,27 @@ module.exports = {
                     name: '小金磨坊',
                     imgSrc: '/image/assets/present/child/1.png',
                     price: 998,
-                    origin: '澳洲'
-                },
-                {
-                    id: 2,
-                    name: '小金磨坊2',
-                    imgSrc: '/image/assets/present/child/2.png',
-                    price: 999,
-                    origin: '澳洲'
+                    origin: '澳洲',
+                    detail: {
+                        carouselMaterial: [
+                            {
+                                imgSrc: '/image/assets/present/child/1/carousel/1.png',
+                                href: ''
+                            },
+                            {
+                                imgSrc: '/image/assets/present/child/1/carousel/2.png',
+                                href: ''
+                            },
+                            {
+                                imgSrc: '/image/assets/present/child/1/carousel/3.png',
+                                href: ''
+                            }
+                        ],
+                        contain: '鱼油（1）＋葡萄籽（2）',
+                        detailImageSrc: '/image/assets/present/child/1/detail.png',
+                        identifyImageSrc: '/image/assets/present/child/1/identify.png',
+                        resourceImageSrc: '/image/assets/present/child/1/resource.png'
+                    }
                 }
             ]
         },
@@ -69,14 +109,27 @@ module.exports = {
                     name: '特产磨坊',
                     imgSrc: '/image/assets/present/special/1.png',
                     price: 998,
-                    origin: '澳洲'
-                },
-                {
-                    id: 2,
-                    name: '特产磨坊2',
-                    imgSrc: '/image/assets/present/special/2.png',
-                    price: 999,
-                    origin: '澳洲'
+                    origin: '澳洲',
+                    detail: {
+                        carouselMaterial: [
+                            {
+                                imgSrc: '/image/assets/present/special/1/carousel/1.png',
+                                href: ''
+                            },
+                            {
+                                imgSrc: '/image/assets/present/special/1/carousel/2.png',
+                                href: ''
+                            },
+                            {
+                                imgSrc: '/image/assets/present/special/1/carousel/3.png',
+                                href: ''
+                            }
+                        ],
+                        contain: '鱼油（1）＋葡萄籽（2）',
+                        detailImageSrc: '/image/assets/present/special/1/detail.png',
+                        identifyImageSrc: '/image/assets/present/special/1/identify.png',
+                        resourceImageSrc: '/image/assets/present/special/1/resource.png'
+                    }
                 }
             ]
         }
