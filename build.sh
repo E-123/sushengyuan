2e0a8227193fb238061ffbeda703f0693fb5f7ef
ibuildthehealth <ibuildthehealth@easylifewell.com>
2016-12-12 11:12:41 +0000

    fix builc.sh: only use for dev
    Signed-off-by: ibuildthehealth <ibuildthehealth@easylifewell.com>

--- build.sh
+++ build.sh
@@ -1,8 +1,3 @@
-
-tar -zxf node_modules.tar.gz
-
-echo "====== node_modules unpacked ======"
-
 node ./node_modules/webpack/bin/webpack --config ./client/webpack-production.config.js
 
 echo "====== webpack build finished ======"
@@ -11,6 +6,6 @@ node ./node_modules/babel-cli/bin/babel server/src -d server/build
 
 echo "====== babel build server finished ======"
 
-NODE_ENV="prod" nohup node server/build/app.js &
+NODE_ENV="prod" node server/build/app.js
 
 echo "====== node start ======"
